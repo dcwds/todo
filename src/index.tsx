2e537ac014d21eca1746ccb2d5b08a024fb8b1e3
Dalton Woods <dcwds@protonmail.com>
2021-01-04 16:59:47 -0500

    chore: add prettier, husky, lint-staged to use with githooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './App';
+import React, { StrictMode } from "react"
+import { render } from "react-dom"
+import "./index.css"
+import App from "./App"
 import { store } from "./store"
-import { Provider } from "react-redux" 
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux"
+import reportWebVitals from "./reportWebVitals"
 
 render(
   <StrictMode>
@@ -12,10 +12,10 @@ render(
       <App />
     </Provider>
   </StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
